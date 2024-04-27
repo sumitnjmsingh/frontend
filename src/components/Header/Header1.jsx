8ff4404eae9187ee15da849e8c15bc213c979325
sumitnjmsingh <sumitsamsingh1111@gmail.com>
2024-04-27 12:12:01 +0530

    responsive cart

--- src/components/Header/Header1.jsx
+++ src/components/Header/Header1.jsx
@@ -23,30 +23,30 @@ function Header1({count}) {
         <div className='bg-[url("offer3.png")] bg-[no-repeat] bg-contain bg-center col-start-4 '></div>
     </div>
 
-    <div className=' h-[60px] grid grid-cols-[40px_4fr_3fr_40px] sticky top-0 left-0 bg-[#f0dada] z-[234]  '>
+    <div className=' h-[60px] grid grid-cols-[40px_4fr_3fr_40px] st:grid-cols-[40px_2fr_5fr_40px] sticky top-0 left-0 bg-[#f0dada] z-[234]  '>
         <div className=' col-start-2'></div>
-        <div className=' col-start-3 grid grid-cols-5 gap-x-[5px]'>
+        <div className=' col-start-3 grid grid-cols-5 st:grid-cols-[2fr_1fr_1fr_1fr_1fr] gap-x-[5px]'>
 
             <div className=' grid grid-cols-[3fr_1fr] place-items-center'>
-                <div><input type="text" className=' lg:w-[65px] md:w-[40px] rounded-[20px] border-[solid] border-[black] border-[2px] p-[2px] bg-[#f0dada]' placeholder='Enter food...'></input></div>
+                <div><input type="text" className='lg:w-[65px] st:w-[40px] md:w-[45px] rounded-[20px] border-[solid] border-[black] border-[2px] p-[2px] bg-[#f0dada]' placeholder='Enter food...'></input></div>
                 <div className='lg:text-[30px] md:text-[20px]'><FaSearch /></div>
             </div>
             <div className='  grid grid-cols-[3fr_1fr] place-items-center '>
-                <div>Latest Offers</div>
+                <div className="st:text-[8px]">Latest Offers</div>
                 <div className='lg:text-[30px] md:text-[20px] ' ><BiSolidOffer /></div>
             </div>
             <div className=' grid grid-cols-[3fr_1fr] place-items-center'>
-                <div>Help Center</div>
+                <div className="st:text-[8px]">Help Center</div>
                 <div className='lg:text-[30px] md:text-[20px]'><IoMdHelpBuoy /></div>
             </div>
             <div className=' grid grid-cols-[3fr_1fr] place-items-center'>
-                <div>
+                <div className="st:text-[8px]">
                     Log In
                     </div>
                 <div className='lg:text-[30px] md:text-[20px]'><RiContactsFill /></div>
             </div>
             <div className=' grid grid-cols-[3fr_1fr] place-items-center'>
-                <div>Cart</div>
+                <div className="st:text-[8px]">Cart</div>
                 <div className='text-[40px] flex relative'><TiShoppingCart /><div className=' text-white font-semibold border-[solid] border-[2px] border-[red] flex text-[15px] w-[19px] h-[19px] justify-center items-center rounded-full bg-[red] absolute top-0 right-0'>{count}</div></div>
             </div>
         </div>
